6edf67c11389f42f3d9f66088e2771f30164f220
JulioCmm <50126932+JulioCmm@users.noreply.github.com>
2025-07-20 15:47:48 -0600

    Update vite.config.js
    Fixed GithubCompat

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/pizza_cake/', // 👈 Esta línea hace que GitHub Pages funcione bien
   plugins: [react()],
 })
