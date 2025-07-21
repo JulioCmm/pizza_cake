0918d095a9554ed177f9e694ab6e299b2f88e2ac
JulioCmm <50126932+JulioCmm@users.noreply.github.com>
2025-07-20 19:48:36 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,4 @@
 import React, { useState } from "react";
-
-import Management3 from './Management3.jpg';
-import Managementog from './ManagementOG.png';
-import Mechanic from './Management2.jpg';
-import Mechanical1 from './Management2.jpg';
-import Mechanical7 from './Mechanical7.jpeg';
-import Mechanical8 from './Mechanical8.jpeg';
-import Mechanical9 from './Mechanical9.png';
-import Mechanical10 from './Mechanical10.png';
-import Mechanical11 from './Mechanical11.png';
-import Mechanical12 from './Mechanical12.png';
-import Mechanical13 from './Mechanical13.png';
-import Photo from './PHOTO.jpeg';
-import Research1 from './Research1.png';
-import Research2 from './Research2.png';
-import Research3 from './Research3.png';
-import Research4 from './Research4.png';
 const sectionContent = {
   "Coding": [
     {
