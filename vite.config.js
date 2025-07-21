c0f45179159febf4966c4e6dfe5a90ba2e0d0de5
JulioCmm <50126932+JulioCmm@users.noreply.github.com>
2025-07-20 18:13:33 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/',
+  base: '/',  // nada más
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
 })
