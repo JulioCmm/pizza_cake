497379a03457f0c0c63f7f02d2403f7711234ecc
JulioCmm <50126932+JulioCmm@users.noreply.github.com>
2025-07-20 19:38:57 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import bowling from './BOWLING.jpeg';
 import broncoSport from './Management2.jpg';
 import coding from './Coding.png';
-import coding1 from './coding1.png';
+import coding1 from './Coding1.png';
 import coding3 from './Coding3.png';
 import coding4 from './Coding4.jpeg';
 import coding6 from './Coding6.png';
